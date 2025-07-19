be7ade7d872a46d16b226bdc7b493937e0074cca
Eduardo <eduardo.olvreis@gmail.com>
2025-07-19 13:17:54 -0300

    Função do scroll inicial concluida

--- script.js
+++ script.js
@@ -10,4 +10,42 @@ window.addEventListener('scroll', function() {
 });
 
 //Evento para scrollar o inicio da página
+let scrolling = false
+let lastScroll = 0
 
+window.addEventListener("scroll", function () {
+    let currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+    if (currentScroll < lastScroll) { //Scrollou pra cima
+        scrollUp();
+    } else{                           //Scrolou pra baixo
+        scrollDown();
+    }
+
+    lastScroll = currentScroll <= 0 ? 0 : currentScroll; // Evita numeros negativos
+});
+
+function scrollUp(){  
+    const header = document.getElementById("topo")
+    const alturaHeader = header.offsetHeight
+    if(window.scrollY <= alturaHeader && !scrolling){
+        scrolling = true
+        window.scrollTo({ top: 0, behavior: "smooth"})
+        setTimeout(() => {
+            scrolling = false
+        }, 1000)
+    }
+    console.log(alturaHeader)
+}
+
+function scrollDown(){
+    const header = document.getElementById("topo")
+    const alturaHeader = header.offsetHeight
+    if(window.scrollY < (alturaHeader) && !scrolling){
+        scrolling = true
+        window.scrollTo({ top: alturaHeader, behavior: 'smooth' });
+        setTimeout(() => {
+            scrolling = false
+        }, 1000)
+    }
+}
